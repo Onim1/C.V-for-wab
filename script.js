cbdcf98331d1d7a21ac01a87605ed16efb8fa326
Dewan M <89687855+Onim1@users.noreply.github.com>
2024-06-25 23:27:40 +0900

    git add cv/Dewan-S-English.pdf

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ document.addEventListener('DOMContentLoaded', function() {
     // Start the animation
     animateHue();
 });
+
+
+// Get the dropdown button
+var dropbtn = document.querySelector(".dropbtn");
+
+// Toggle the display of the dropdown content on click
+dropbtn.onclick = function() {
+  document.querySelector(".dropdown-content").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}

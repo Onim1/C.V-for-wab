a47761cca63f0dbc1d5754fea28f8978c72c5bd1
Dewan M <89687855+Onim1@users.noreply.github.com>
2024-06-30 22:53:07 +0900

    git commit -m "Update style.css and script.js"

--- script/script.js
+++ script/script.js
@@ -42,3 +42,12 @@ window.onclick = function(event) {
     }
   }
 }
+
+document.querySelector('.dropbtn').addEventListener('click', function() {
+  var content = this.nextElementSibling;
+  if (content.style.maxHeight){
+      content.style.maxHeight = null;
+  } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+  } 
+});
